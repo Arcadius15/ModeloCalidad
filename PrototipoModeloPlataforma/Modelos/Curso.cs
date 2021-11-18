3ced95379df58fd14963520bfff5c537ba13746a
Diego Vicente <diegovic99@hotmail.com>
2021-11-17 22:41:24 -0500

    Version 1

---
+++ PrototipoModeloPlataforma/Modelos/Curso.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototipoModeloPlataforma.Modelos
+{
+    public class Curso
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string nombre { get; set; }
+        [Required]
+        public int creditos { get; set; }
+    }
+}
