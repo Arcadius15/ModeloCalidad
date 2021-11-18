33e823765a5e2dc042e063679c365d1500911bd9
Diego Vicente <diegovic99@hotmail.com>
2021-11-17 22:41:24 -0500

    Version 1

---
+++ PrototipoModeloPlataforma/Modelos/Usuario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototipoModeloPlataforma.Modelos
+{
+    public class Usuario
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        [MaxLength(15)]
+        public string usuario { get; set; }
+        [Required]
+        [MaxLength(15)]
+        public string contraseña { get; set; }
+        [MaxLength(12)]
+        public string nombre { get; set; }
+        [MaxLength(30)]
+        public string apellido { get; set; }
+        public int edad { get; set; }
+        [Required]
+        [MaxLength(8)]
+        [MinLength(8)]
+        public string dni { get; set; }
+        public DateTime nacimiento { get; set; }
+        [Required]
+        public int tipo { get; set; }
+    }
+}
